5121b47d7bd2346606f52222e6d4547644738c52
Mbah Emmanuel <105330079+shadowstiles@users.noreply.github.com>
2022-11-25 01:07:33 -1200

    Update specifier.c

--- specifier.c
+++ specifier.c
@@ -11,38 +11,48 @@
 
 void specifier(char c, va_list ap)
 {
-	char *a;
 	long b;
 	int i;
-
-	if (c == 'd' || c == 'i')
+	
+	switch (c)
 	{
-		b = va_arg(ap, long);
-		number(b);
-	}
+		case 'd':
+		case 'i':
+			b = va_arg(ap, int);
+			number(b);
+			break;
+		case 'c':
+			_putchar(va_arg(ap, int));
+			break;
+		case 's':
+			handleString(ap);
+			break;
+		case 'b':
+			binary(va_arg(ap, long));
+			break;
+		case 'o':
+			octal(va_arg(ap, long));
+			break;
+		case 'x':
+			hexa(va_arg(ap, long), 97);
+			break;
+		case 'X':
+			hexa(va_arg(ap, long), 65);
+			break;
+		default:
+			_putchar('%');
+			_putchar(c);
+    }
+}
 
-	if (c == 'c')
-		_putchar(va_arg(ap, int));
-
-	if (c == 's')
-	{
-		a = va_arg(ap, char *);
-		if (a == 0)
-			a = "(null)";
-
-		for (i = 0; a[i] != '\0'; i++)
-			_putchar(a[i]);
-	}
-
-	if (c == 'b')
-		binary(va_arg(ap, long));
-
-	if (c == 'o')
-		octal(va_arg(ap, long));
-
-	if (c == 'x')
-		hexa(va_arg(ap, long), 97);
-
-	if (c == 'X')
-		hexa(va_arg(ap, long), 65);
+void handleString(va_list ap)
+{
+	char *a;
+	
+	a = va_arg(ap, char *);
+	if (a == 0)
+		a = "(null)";
+	
+	for (i = 0; a[i] != '\0'; i++)
+		_putchar(a[i]);
 }
