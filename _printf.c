2ba2863a16077e2d904f5f232ddb65123bc0d289
Mbah Emmanuel <105330079+shadowstiles@users.noreply.github.com>
2022-11-26 05:40:47 -1200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -35,7 +35,7 @@ int _printf(const char *format, ...)
 			else
 			{
 				if (format[i + 1] == '\0')
-					return (0);
+					return (-1);
 				else
 					count = count + specifier(format[i + 1], arglist);
 			}
