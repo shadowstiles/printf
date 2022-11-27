126413b6982fd3ee8b2aafa32fc68cc23b5b2f62
shadowstiles <successforemmy@gmail.com>
2022-11-27 01:20:37 -0800

    work on other _printf

--- _printf.c
+++ _printf.c
@@ -16,6 +16,9 @@ int _printf(const char *format, ...)
 	int i;
 	int count = 0;
 	va_list arglist;
+	char formatSpecifier[8] = {'u', 'i', 'x', 'X', 'o', 'd', 'b'};
+	char formatSpecifier2[8] = {'s', 'S', 'c', 'r', 'R', 'p', '%'}
+	char customSpecifier[8] = {'+', '-', ' ', '0', 'l', 'h', '#'};
 
 	if (format == NULL)
 		return (-1);
@@ -24,19 +27,33 @@ int _printf(const char *format, ...)
 
 	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[i] == '%' && format[i + 1] != ' ')
+		if (format[i] == '%')
 		{
-			if (format[i + 1] == '%')
+			for (j = 0; j < 8; j++)
 			{
-				_putchar(format[i + 1]);
-				count++;
-			}
-			else
-			{
-				if (format[i + 1] == '\0')
-					return (0);
+				switch (format[i + 1])
+				{
+					case formatSpecifier[j]:
+						count += specifier(format[i + 1], arglist);
+						break;
+					case formatSpecifier2[j]:
+						count += specifier2(format[i + 1], arglist);
+						break;
+					case customSpecifier[j]:
+						count += custom_specifier(format[i + 1], format[i + 2], arglist);
+						break;
+					default:
+						for (k = i; format[k + 1] > 48 && format[k + 1] < 58; k++)
+						{
+							if (result < 0)
+								result = format[k + 1];
+							else
+								result = (result * 10) + format[k + 1];
+						
+				else
+				{
 
-				count = count + specifier(format[i + 1], arglist);
+					count += other_specifier(
 			}
 			i++;
 		}
